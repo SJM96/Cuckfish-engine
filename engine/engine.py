e50a5d3f9dcc954228a229585593c5d8cdb69229
Joonatan Mäkinen <joonatanrocks@gmail.com>
2021-07-30 02:20:38 +0300

    add depth search

--- engine/engine.py
+++ engine/engine.py
@@ -8,9 +8,10 @@ from pathlib import Path
 class ChessEngine:
     """The artificial system that calculates the next move for the current game state."""
 
-    def __init__(self, side: chess.Color):
+    def __init__(self, side: chess.Color, depth: int):
         """Initialize the playing side and the opening book."""
         self.side = chess.WHITE if side == "White" else chess.BLACK
+        self.depth = depth
         try:
             ob_file = Path(__file__).parent / "openings.bin"  # Insert name of opening book here
             self.opening_book = chess.polyglot.open_reader(ob_file)
@@ -26,7 +27,6 @@ class ChessEngine:
         """
         # All the currently legal moves
         uci_moves = list(board.legal_moves)
-        san_moves = [board.san(move) for move in uci_moves]
 
         # Play a move from the opening book if book suggests one
         if self.opening_book:
@@ -37,37 +37,34 @@ class ChessEngine:
                 click.echo("This is from the opening book.")
                 return random.choice(opening_moves[:3])
 
-        # curr_score = self.evaluate_board(board)
-        # remaining_moves = san_moves.copy()
-        evaluated_moves = []
-        for move in san_moves:
-            test_board = board.copy()
-            test_board.push_san(move)
-            new_uci_moves = list(test_board.legal_moves)
-            new_san_moves = [test_board.san(new_move) for new_move in new_uci_moves]
-            best_opp_score = -1000
-            for opp_move in new_san_moves:
-                new_test_board = test_board.copy()
-                new_test_board.push_san(opp_move)
-                score = self.evaluate_board(new_test_board)
-                # if self.side is chess.WHITE:
-                #   score *= -1
-                if score > best_opp_score:
-                    best_opp_score = score
-            evaluated_moves.append((move, best_opp_score))
+        # Search for move values
+        depth = self.depth
+        maxScore = -1000
+        move_evals = []
+        with click.progressbar(uci_moves, label="Calculating", length=len(uci_moves)) as bar:
+            # Amazing loading bar progresses with each calculated move
+            for move in bar:
+                board.push(move)
+                score = -self.search(board, depth - 1)
+                if score > maxScore:
+                    maxScore = score
+                move_evals.append((move, score))
+                board.pop()
+            move_evals.sort(key = lambda x: x[1])
+            move_evals.reverse()
 
-        evaluated_moves.sort(key=lambda x: x[1])
-        # evaluated_moves.reverse()
-        click.echo(evaluated_moves)
+        # Rank worthiest moves
         remaining_moves = []
-        remaining_moves.append(evaluated_moves.pop(0))
-        for i in evaluated_moves:
-            if remaining_moves[0][1] < i[1]:
+        for i in move_evals:
+            if move_evals[0][1] == i[1]:
+                remaining_moves.append(board.san(i[0]))
+            else:
                 break
-            remaining_moves.append(i)
+
+        # Pick one of the worthiest moves
         random.shuffle(remaining_moves)
         click.echo(remaining_moves)
-        return remaining_moves[0][0]
+        return remaining_moves[0]
 
     def check_opening_sequence(self, board: chess.Board) -> list:
         """Get all next moves suggested by an opening book.
@@ -81,7 +78,7 @@ class ChessEngine:
             opening_moves.append(move_san)
         return opening_moves
 
-    def evaluate_board(self, board: chess.Board) -> float:
+    def evaluate_board(self, board: chess.Board, color: chess.Color) -> float:
         """Calculate current game score.
 
         :param board: Current state of board
@@ -92,4 +89,21 @@ class ChessEngine:
         for i in range(1, 6):
             total += len(board.pieces(i, chess.WHITE)) * vals[i]
             total -= len(board.pieces(i, chess.BLACK)) * vals[i]
-        return total
+        return total if color else total * -1
+
+    def search(self, board: chess.Board, depth: int) -> int:
+        """Search move values at specific depth.
+
+        :param board: Current state of board
+        :param depth: Depth of search
+        """
+        if depth == 0:
+            return self.evaluate_board(board, board.turn)
+        maxScore = -1000
+        for move in board.legal_moves:
+            board.push(move)
+            score = -self.search(board, depth - 1)
+            if score > maxScore:
+                maxScore = score
+            board.pop()
+        return maxScore
